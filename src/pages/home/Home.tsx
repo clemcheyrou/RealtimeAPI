f2986d497d62ce51f59d5c598c244f46ac92a2bd
clemcheyrou <clemcheyrou@gmail.com>
2024-11-13 00:19:31 +0100

    feat(wip)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,12 @@
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <h1>
+        Simplifiez la création 
+        de votre Entreprise 
+        grâce à l'IA
+      </h1>
+    </div>
   )
 }
