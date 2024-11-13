fb5babe36d130e54a9faae0a54f5659dcad68153
Elise Maleuvre <elise.maleuvre@gmail.com>
2024-11-13 04:55:46 +0100

    UPDATE: basic object with shape keys for mouth opening BUT no way to make it functional on threeJS I have to sleep Im sorry will debug tomorrow

--- src/pages/test/Testr3f.tsx
+++ src/pages/test/Testr3f.tsx
@@ -1,6 +1,35 @@
+import React, { useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { useGLTF } from '@react-three/drei';
+import * as THREE from 'three';
+
+const DisplayGLTF = () => {
+  const { nodes } = useGLTF('../faceTestWithMorphTargets.glb');
+
+  useEffect(() => {
+    console.log('nodes.head:', nodes.head);
+  }, [nodes]);
+
+  const headMesh = nodes.head as THREE.Mesh | undefined;
+
+  if (!headMesh || !headMesh.geometry) {
+    return null;
+  }
+
+  return (
+    <mesh geometry={headMesh.geometry}>
+      <meshStandardMaterial color="orange" />
+    </mesh>
+  );
+};
 
 export const Testr3f = () => {
   return (
-    <div>Testr3f</div>
-  )
-}
+    <div style={{ height: '100vh' }}>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <DisplayGLTF />
+      </Canvas>
+    </div>
+  );
+};
